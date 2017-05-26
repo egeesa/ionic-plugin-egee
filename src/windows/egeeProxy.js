5d7e8832798f3637ea4489d9374d4305f7cccc82
Vincent AUGEROT <vau@egee.fr>
2017-05-26 17:05:35 +0200

    Test

--- src/windows/egeeProxy.js
+++ src/windows/egeeProxy.js
@@ -2,22 +2,51 @@
  * @author e-GEE SA 
  */
 
-function Egee () {
-    this._promise = null;
-    this._cancelled = false;
-}
+// function Egee () {
+//     this._promise = null;
+//     this._cancelled = false;
+// }
 
 
-var egeeProxy = {
+// var egeeProxy = {
 
+//     helloworld: function (success, error, message) {
+//         alert('test windows');
+//         //Créer une instance de la classe CS
+//         var classe = new Egee.Egee();
+//         alert('test windows 2');
+
+//         return classe.HelloWorld(message);
+//     }
+// };
+
+cordova.commandProxy.add("Egee", {
     helloworld: function (success, error, message) {
         alert('test windows');
         //Créer une instance de la classe CS
         var classe = new Egee.Egee();
         alert('test windows 2');
-        
-        return classe.HelloWorld(message);
-    }
-};
 
-cordova.commandProxy.add("Egee", egeeProxy);
+        return classe.HelloWorld(message);
+        //  +            var file = args[0];
+        //  +            var dir = args[1];
+        //  +
+        //  +            var plugin = ZipCSComponent.ZipPlugin();
+        //  +
+        //  +            plugin.unZipByUriAsync(file, dir, function (loaded, total) {
+        //  +            //plugin.unZipByPathAsync(file, dir, function (loaded, total) {
+        //  +                var progressEvent = new ProgressEvent("OK", { loaded: loaded, total: total })
+        //  +                var callbackParams = { keepCallback: true };
+        //  +                successCallback(progressEvent, callbackParams);
+        //  +            })
+        //  +            .done(
+        //  +                function () {
+        //  +                    successCallback();
+        //  +                },
+        //  +                function (error) {
+        //  +                    errorCallback(error);
+        //  +                }
+        //  +            );
+
+    }
+}); 
