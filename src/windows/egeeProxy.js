6fb6a3bbf18c81b7bdb58cb938399d5dd6b63ac8
Vincent AUGEROT <vau@egee.fr>
2017-05-26 15:55:02 +0200

    Test windows 10

--- src/windows/egeeProxy.js
+++ src/windows/egeeProxy.js
@@ -10,9 +10,12 @@ function Egee () {
 
 var egeeProxy = {
 
-    helloworld: function (message, successCallback, errorCallback, options) {
+    helloworld: function (success, error, message) {
+        alert('test windows');
         //Créer une instance de la classe CS
         var classe = new Egee.Egee();
+        alert('test windows 2');
+        
         return classe.HelloWorld(message);
     }
 };
