658253be2984916490f3734520d46b6814b5e4fe
Guy LAGUEU <gla@egee.fr>
2017-07-20 09:03:18 +0200

    test plugin

--- src/windows/_lib/Egee/SappelProxy.cs
+++ src/windows/_lib/Egee/SappelProxy.cs
@@ -4,18 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using IZAR_CSIXLib;
+using System.Runtime.InteropServices;
 
 namespace Egee.Proxy
 {
-    public  class SappelProxy
+    public sealed class SappelProxy
     {
-        public  string getVersion()
+  
+        public string getVersion()
         {
             return call("return Environment.getVersion()");
         }
 
-        private  string call(string commande)
-        {           
+        private string call(string commande)
+        {
             HyScript api = new HyScript();
             string result = api.call(commande);
             return result;
