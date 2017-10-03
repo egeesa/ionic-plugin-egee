91365c11d30f55d15664ab72ddef76e9c3e7e87b
Guy LAGUEU <gla@egee.fr>
2017-10-03 15:38:37 +0200

    Création projet Test unitaire

---
+++ src/windows/_lib/Egee.Proxy.Test/EgeeProxyTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+//using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Xunit;
+
+namespace Egee.Proxy.Test
+{
+    public class EgeeProxyTest
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            string version = EgeeProxy.getversion();
+            Assert.NotNull(version);
+        }
+    }
+}
